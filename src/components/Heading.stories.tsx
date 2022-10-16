ab66f09aa5097f7fe67b9a602c98a715a980af72
AmandaCacholi <amanda.cacholi@outlook.com>
2022-10-16 12:54:22 -0300

    feat: add project to github

---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Heading, HeadingProps } from "./Heading";
+
+export default {
+  title: "Components/Heading",
+  component: Heading,
+  args: {
+    children: "Lorem ipsum",
+    size: "sm",
+  },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj<HeadingProps> = {};
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+  args: {
+    asChild: true,
+    children: <h1>Text with h1 tag</h1>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
